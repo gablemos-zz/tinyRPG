20894a993bea3d395b3a16331e83cf215c68fd51
Gabriel Lemos <x484098@gruposantander.com>
2021-09-21 23:06:36 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+// Require the necessary discord.js classes
+import { Client, Intents } from 'discord.js';
+import { token } from './config/config.json';
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+console.log(token);
+// When the client is ready, run this code (only once)
+client.on('ready', () => {
+  console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login(token);
