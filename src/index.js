17953cb46e679ed5e4bb2bc0e1c5c0676618a2fa
Gabriel Lemos <x484098@gruposantander.com>
2021-09-21 22:41:24 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Running');
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config/config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login(token);
